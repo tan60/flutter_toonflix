6a974b2a5fc72c72f9c5cbace204722f7f9fed7a
dhkim <dohyoung81@gmail.com>
2023-02-11 10:48:24 +0900

    BuildContext - 부모 theme 접근

--- lib/main.dart
+++ lib/main.dart
@@ -12,43 +12,47 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
-
   void onClicked() {
-    setState(() {
-      counter = counter + 1; // setState 안에 넣어야 코드 가독성이 좋다.
-    });
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Click Count',
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              Text(
-                '$counter',
-                style: const TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              IconButton(
-                  onPressed: onClicked,
-                  icon: const Icon(
-                    Icons.add_box_rounded,
-                  ))
+            children: const [
+              MyLargetTitle(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargetTitle extends StatelessWidget {
+  const MyLargetTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge!.color,
       ),
     );
   }
