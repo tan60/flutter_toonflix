042bd3a7112ae5ce5291d42d5d6252d667be3edf
dhkim <dohyoung81@gmail.com>
2023-02-16 23:20:11 +0900

    리스트 및 상세화면 적용

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,21 +4,23 @@ import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
 
-  Future<List<WebtoonModel>> getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     print(url);
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      webtoonInstances = jsonDecode(response.body);
+      final webtoons = jsonDecode(response.body);
 
-      for (var webtoon in webtoonInstances) {
+      for (var webtoon in webtoons) {
         //final toon = WebtoonModel.fromJson(webtoon);
-        print(webtoon.title);
+        final instance = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(instance);
       }
       return webtoonInstances;
     }
